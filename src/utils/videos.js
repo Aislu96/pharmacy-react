3fc7ab6dc8e273893bd7d3befc893d8b6b5f4f29
aislusha96 <arslanova1996ais@list.ru>
2024-07-16 19:11:54 +0300

    commit

--- src/utils/videos.js
+++ src/utils/videos.js
@@ -1,38 +1,46 @@
 import triangle from "../images/video/triangle.svg";
+import play from "../images/video/play.svg";
 
 const videos = [
     {
         "video": "https://www.youtube.com/embed/OVziwaUQZ-c",
         "src": triangle,
+        "srcPlay": play,
         "title": "Видео про биовестин"
     },
     {
         "video": "https://www.youtube.com/embed/vB36CThAen0",
         "src": triangle,
+        "srcPlay": play,
         "title": "Видео про биовестин"
     },
     {
         "video": "https://www.youtube.com/embed/OVziwaUQZ-c",
         "src": triangle,
+        "srcPlay": play,
         "title": "Видео про биовестин"
     },
     {
         "video": "https://www.youtube.com/embed/vB36CThAen0",
         "src": triangle,
+        "srcPlay": play,
         "title": "Видео про биовестин"
     },
     {
         "video": "https://www.youtube.com/embed/OVziwaUQZ-c",
         "src": triangle,
+        "srcPlay": play,
         "title": "Видео про биовестин"
     }, {
         "video": "https://www.youtube.com/embed/vB36CThAen0",
         "src": triangle,
+        "srcPlay": play,
         "title": "Видео про биовестин"
     },
     {
         "video": "https://www.youtube.com/embed/OVziwaUQZ-c",
         "src": triangle,
+        "srcPlay": play,
         "title": "Видео про биовестин"
     }
 ]
